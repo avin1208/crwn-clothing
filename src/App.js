d9376dac04e011cce6be36f05c64c6df63f9f86b
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-02-11 14:43:09 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/hompage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-    <HomePage/>
-    </div>
+      <Switch>
+         <Route exact path='/' component={HomePage} />
+         <Route path='/hats' component={HatsPage} />
+      </Switch>
+  </div>  
   );
 }
-
+ 
 export default App;
