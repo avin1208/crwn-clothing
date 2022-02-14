525ef0a9e68c3a913658301adab28a19da8068e8
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-02-14 10:19:00 +0530

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import HomePage from './pages/homepage/hompage.component';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 
+import Header from './components/header/header.component.jsx';
+
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
          <Route exact path='/' component={HomePage} />
          <Route path='/shop' component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,4 @@
-import Recat from 'react';
+import React from 'react';
 
 import './collection-item.styles.scss';
 
