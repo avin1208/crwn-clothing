e924eb1844ca675f3e9ef3b81a193558f8a5e8a4
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-03-01 12:52:39 +0530

    updated

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -29,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
-            <Link className='option' to='/shop'>
+            <Link className='option' to='/contact'>
                 CONTACT
             </Link>
             {
