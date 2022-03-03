8e8cf632b1c165be3c90c5a10fd23ab82e49bbe0
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-03-03 11:55:40 +0530

    updated component styled like header component, custom-button component, homepage component and our shop data store in firebase

--- src/App.js
+++ src/App.js
@@ -26,11 +26,12 @@ import { setCurrentUser } from './redux/user/user.actions';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 
+
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser} = this.props; 
+    const { setCurrentUser } = this.props; 
 
 
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/compat/app';
+ import firebase from 'firebase/compat/app';
 
 import 'firebase/compat/firestore';
 
@@ -39,7 +39,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    
 
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+      return await batch.commit();
+};
 
 
 firebase.initializeApp(config);
